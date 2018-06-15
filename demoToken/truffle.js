422e9d3c7ff3c232efc24a85b479f2f672174d4a
esotericzhang <zhange2508@gmail.com>
2018-06-15 15:23:02 -0500

    Truffle config file update
    Updated the truffle configuration file to deploy the demo token onto the Travis Testnet

--- demoToken/truffle.js
+++ demoToken/truffle.js
@@ -2,10 +2,13 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration
 	networks: {
-		development: {
-			host: "localhost",
-			port: "7545",
-			network_id: "*"		//match any network id
+		testnet: {
+		host: "localhost",
+		port: 8545,
+		network_id: 3,
+		from: "0xfc60d1d90b112c2e4fa629ac484d40a0c8194e5f",
+		gas: 1000000,
+      	gasPrice: 10000000000
 		}
 	}
 };
