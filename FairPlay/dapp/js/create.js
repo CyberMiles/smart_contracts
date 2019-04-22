034006b95b56ebd847b3fac4dca7d1324bd1efa1
Michael Yuan <michael@michaelyuan.com>
2019-04-22 13:12:07 +0800

    Update create.js

--- FairPlay/dapp/js/create.js
+++ FairPlay/dapp/js/create.js
@@ -12,6 +12,18 @@ $(function () {
     getBin();
     // initLanguage();
 
+    $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, {
+            icons: {
+                time: 'far fa-clock',
+                date: 'far fa-calendar',
+                up: 'far fa-arrow-up',
+                down: 'far fa-arrow-down',
+                previous: 'far fa-chevron-left',
+                next: 'far fa-chevron-right',
+                today: 'far fa-calendar-check-o',
+                clear: 'far fa-trash',
+                close: 'far fa-times'
+    } });
     $('#cutoff').datetimepicker();
 });
 
