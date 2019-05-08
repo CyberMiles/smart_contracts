d7a79d08fb07248166831465b48bae20087421ec
atlasquan <atlasquan98@gmail.com>
2019-05-08 13:52:37 +0800

    change time picker

--- FairPlay/dapp/js/create.js
+++ FairPlay/dapp/js/create.js
@@ -21,21 +21,21 @@ $(function () {
              $('.image-upload-wrap').removeClass('image-dropping');
     });
     // Data Picker Initialization
-    $('.datepicker').pickadate();
-    // $('#cutoff').datetimepicker({
-    //         minDate: moment().add(1, 'm'),
-    //         icons: {
-    //             time: 'far fa-clock',
-    //             date: 'far fa-calendar',
-    //             up: 'fas fa-arrow-up',
-    //             down: 'fas fa-arrow-down',
-    //             previous: 'fas fa-chevron-left',
-    //             next: 'fas fa-chevron-right',
-    //             today: 'far fa-calendar-check-o',
-    //             clear: 'far fa-trash',
-    //             close: 'far fa-times'
-    //         },
-    // });
+    // $('.datepicker').pickadate();
+    $('#cutoff').datetimepicker({
+            minDate: moment().add(1, 'm'),
+            icons: {
+                time: 'far fa-clock',
+                date: 'far fa-calendar',
+                up: 'fas fa-arrow-up',
+                down: 'fas fa-arrow-down',
+                previous: 'fas fa-chevron-left',
+                next: 'fas fa-chevron-right',
+                today: 'far fa-calendar-check-o',
+                clear: 'far fa-trash',
+                close: 'far fa-times'
+            },
+    });
 });
 
 // init language
