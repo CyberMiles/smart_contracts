98522776b5566e7750def04e6694361802d713f4
atlasquan <atlasquan98@gmail.com>
2019-05-08 16:56:33 +0800

    add md of desc

--- FairPlay/dapp/js/create.js
+++ FairPlay/dapp/js/create.js
@@ -115,15 +115,14 @@ var create = function () {
             //          - [123](http1)
             //          - [111333](dd1)
             //          - [2334555](dfd)`
-            desc_md = "# Description\n" 
+            desc_md = "#### Description\n\n" 
             desc_md += desc + "\n\n"
             if (shopping_site.length > 1){
-                desc_md += "# Shopping Link\n"
+                desc_md += "#### Shopping Link\n\n"
                 var i;
                 for(i = 1; i < shopping_site.length; i++){
-                     desc_md += "    - ["+shopping_site[i]+"]("+ shopping_link[i] +")\n"
+                     desc_md += "- ["+shopping_site[i]+"]("+ shopping_site[i] +")\n"
                 }
-                console.log(desc_md)
             }
             
             // desc = "{\"desc\":\"" + desc + "\"}";
