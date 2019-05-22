1bbe96552f56eb64e192d29659890c770f0b02fb
atlasquan <atlasquan98@gmail.com>
2019-05-22 10:32:05 +0800

    to lower case

--- FairPlay/v1/dapp/js/secondStateJS.js
+++ FairPlay/v1/dapp/js/secondStateJS.js
@@ -139,7 +139,7 @@ var IWonButton = () => {
 
 var searchButton =  async () => {
         $('.results').empty()
-        var theAddress = $("#searchAddressInput").val();
+        var theAddress = $("#searchAddressInput").val().toLowerCase();
         var theText = $("#searchTextInput").val();
         //console.log($.trim(theAddress.length));
         if ($.trim(theAddress.length) == "0" && $.trim(theText.length) == "0") {
