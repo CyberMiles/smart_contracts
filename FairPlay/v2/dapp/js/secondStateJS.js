323f0a38b187a462f152d28db5ef0c54eb858088
atlasquan <atlasquan98@gmail.com>
2019-05-30 16:15:28 +0800

    web3.eth => web3.cmt

--- FairPlay/v2/dapp/js/secondStateJS.js
+++ FairPlay/v2/dapp/js/secondStateJS.js
@@ -12,7 +12,7 @@ var publicIp = "https://cmt-testnet.search.secondstate.io";
 var ICreatedButton = () => {
         async function setUpAndProgress() {
             this.currentAccount = "";
-            await web3.eth.getAccounts((err, accounts) => {
+            await web.cmt.getAccounts((err, accounts) => {
                 this.currentAccount = accounts[0]
             });
 
@@ -41,7 +41,7 @@ var IParticipatedButton = () => {
         async function setUpAndProgress() {
             
             this.currentAccount = "";
-            await web3.eth.getAccounts((err, accounts) => {
+            await web.cmt.getAccounts((err, accounts) => {
                 this.currentAccount = accounts[0]
             });
 
@@ -75,7 +75,7 @@ var IWonButton = () => {
         async function setUpAndProgress() {
             var originalState = $("#pb.progress-bar").clone();
             this.currentAccount = "";
-            await web3.eth.getAccounts((err, accounts) => {
+            await web.cmt.getAccounts((err, accounts) => {
                 this.currentAccount = accounts[0]
             });
             
