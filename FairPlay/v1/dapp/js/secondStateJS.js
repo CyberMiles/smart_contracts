427c9253e6de6c7fb697defba7258078b479512c
Michael Yuan <michael@michaelyuan.com>
2019-06-04 10:11:40 -0700

    Update secondStateJS.js

--- FairPlay/v1/dapp/js/secondStateJS.js
+++ FairPlay/v1/dapp/js/secondStateJS.js
@@ -1,8 +1,9 @@
 $.ajaxPrefilter( function (options) {
   if (options.crossDomain && jQuery.support.cors) {
     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    options.url = http + '//cors.io/?' + options.url;
+    // options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    // options.url = "http://cors.corsproxy.io/url=" + options.url;
   }
 });
 
