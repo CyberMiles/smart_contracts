1942e629e93f87dfa2e037a53924daa710eab61f
Michael Yuan <michael@ringful.com>
2019-06-18 02:47:38 -0500

    Updte

--- MarketPlace/v1/dapp/js/listing.js
+++ MarketPlace/v1/dapp/js/listing.js
@@ -193,8 +193,8 @@ var buy = function () {
             var contract_crc20 = web3.cmt.contract(abi_crc20);
             var instance_crc20 = contract_crc20.at(crc20);
             console.log(contract_address);
-            console.log(tokens[crc20]);
-            instance_crc20.approve(contract_address, tokens[crc20], {
+            console.log(parseInt(tokens[crc20]));
+            instance_crc20.approve(contract_address, parseInt(tokens[crc20]), {
                 gas: '200000',
                 gasPrice: 0
             }, function (error, result) {
